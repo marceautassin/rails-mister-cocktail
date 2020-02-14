3c71cf4514ae1bf1180df3a8ff4479f42e121f65
Marceau Tassin <marceau.tassin@gmail.com>
2020-02-14 16:57:22 +0100

    root résolu

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: %w[index show new create] do
+  root "cocktails#index"
+  resources :cocktails, only: %w[show new create] do
     resources :doses, only: %w[new create destroy], shallow: true
   end
 end
